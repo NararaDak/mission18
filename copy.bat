b35aa541a627459d3f84907efbbd2aedb726070e
nabi <nabidream@gmail.com>
2026-03-22 11:11:19 +0900

    주소 수정

--- copy.bat
+++ copy.bat
@@ -1,2 +1,2 @@
 @echo off
-robocopy "D:\project\mission18" "Y:\home\ubuntu\project\mission18" /MIR /MT:8 /R:2 /W:2 /XD .web .venv __pycache__ .states .github .git mlruns .vscode data .streamlit /XF *.pyc
+robocopy "D:\project\mission18" "X:\project\mission18" /MIR /MT:8 /R:2 /W:2 /XD .web .venv __pycache__ .states .github .git mlruns .vscode data .streamlit /XF *.pyc
